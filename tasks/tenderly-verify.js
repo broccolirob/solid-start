51841a7858fb401425e90193c88229d261754655
broccolirob <admin@dfsnft.com>
2021-09-08 13:21:04 -0400

    generic tenderly task

--- tasks/tenderly-verify.js
+++ tasks/tenderly-verify.js
@@ -1,7 +1,19 @@
 module.exports = task(
   "tenderly-verify",
   "Verifies and uploads contracts to Tenderly",
-  async (_, { network }) => {
-    console.log(`Needs to be implemented. Network: ${network.name}`);
+  async (_, { network, tenderly, deployments }) => {
+    if (["hardhat", "localhost"].includes(network.name)) {
+      throw Error("Tenderly verification is not available on local networks.");
+    }
+    const deps = await deployments.all();
+    const contractNames = Object.keys(deps);
+    const contracts = contractNames.map((name) => ({
+      name,
+      network: network.name,
+      address: deps[name].address,
+    }));
+    console.log("Verifying and pushing the following contracts:", contracts);
+    await tenderly.verify(...contracts);
+    await tenderly.push(...contracts);
   }
 );
